5503dce4a72856ba689fe0038cec5ba13ae45493
Ayushi Agarwal <ayushiagg1509@gmail.com>
2024-07-03 12:11:32 +0530

    build error solved

--- my-app/src/components/EditorComponents/Default.jsx
+++ my-app/src/components/EditorComponents/Default.jsx
@@ -4,7 +4,7 @@ export default function Default() {
       <div className="flex flex-col gap-10 items-center justify-center text-center">
         <div>
           <p className="text-4xl font-bold text-black mb-10">Welcome to Wasserstoff IDE</p>
-          <p className="text-2xl font-bold text-black">Let's get Started 🚀 🚀</p>
+          <p className="text-2xl font-bold text-black">Let&apos;s get Started 🚀 🚀</p>
         </div>
       </div>
     </div>
